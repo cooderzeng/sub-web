c2e6fac47d9a13265a40a5585771fa5d8f2f16a5
coder <48429942+cooderzeng@users.noreply.github.com>
2024-07-04 22:59:40 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -292,7 +292,7 @@ export default {
       form: {
         sourceSubUrl: "",
         clientType: "",
-        customBackend: window.location.origin,
+        customBackend: `${window.location.origin}/sub?`,
         remoteConfig: "",
         excludeRemarks: "",
         includeRemarks: "",
